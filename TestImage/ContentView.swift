420738af37df1613000bfed7c2bb6b22d23d7ef3
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-08-08 14:40:50 +0400

    Add all functions

--- TestImage/ContentView.swift
+++ TestImage/ContentView.swift
@@ -14,12 +14,13 @@ class E2P {
     private let frame: UIImage
     private let equ_w: Int
     private let equ_h: Int
-    private let focus_of_view: Double // Add this property
-    private let width: Int // Add this property
-    private let height: Int // Add this property
-    private let wFOV: CGFloat // Add this property
-    private let hFOV: CGFloat // Add this property
-    
+    private let focus_of_view: Double 
+    private let width: Int
+    private let height: Int
+    private let wFOV: CGFloat
+    private let hFOV: CGFloat
+   
+
     
     
     init(originalImage: UIImage, fov: Double) {
@@ -27,27 +28,13 @@ class E2P {
         self.equ_w = Int(originalImage.size.width)
         self.equ_h = Int(originalImage.size.height)
         self.focus_of_view = fov
-        self.width = 1600 // Set your desired width
-        self.height = 1600 // Set your desired height
+        self.width = 1400
+        self.height = 1400
         self.wFOV = CGFloat(fov) // Set the FOV for wFOV
         self.hFOV = CGFloat(self.height) / CGFloat(self.width) * wFOV
-        // Set other properties and initialization logic here
-    }
+       
 
-//    private func combinedMatrix() -> [CGPoint] {
-//        var points = [CGPoint]()
-//
-//        // Примерная логика для заполнения массива точек
-//        for row in 0..<height {
-//            for col in 0..<width {
-//                let x = CGFloat(col) / CGFloat(width) * 2 - 1
-//                let y = CGFloat(row) / CGFloat(height) * 2 - 1
-//                points.append(CGPoint(x: x, y: y))
-//            }
-//        }
-//
-//        return points
-//    }
+    }
 
     private func combinedMatrix() -> [[CGPoint]] {
         var points = [[CGPoint]]()
@@ -94,9 +81,6 @@ class E2P {
         return CGPoint(x: x, y: y)
     }
 
-
-
-    
     private func bilinearInterpolationMatrix(lon: Double, lat: Double) -> [[CGPoint]] {
         let ufXCoords = lon
         let vfYCoords = lat
@@ -106,8 +90,8 @@ class E2P {
         
         var combinedMatrix = [[CGPoint]](repeating: [CGPoint](repeating: CGPoint.zero, count: width), count: height)
         
-        for y in 0..<height {
-            for x in 0..<width {
+        for y in 0..<equ_h {
+            for x in 0..<equ_w {
                 let index = y * width + x
                 
                 xxDense[index] = CGFloat(ufXCoords)
@@ -126,10 +110,30 @@ class E2P {
         }
         return combinedMatrix()[y][x]
     }
+    
+    
+    private func convertOriginalToFOVCoordinates(x: Int, y: Int) -> CGPoint {
+        guard y >= 0 && y < height && x >= 0 && x < width else {
+            return .zero
+        }
+        
+        var point = CGPoint(x: CGFloat(x), y: CGFloat(y))
+        point = bilinearInterpolationForPointPair(point: point)
+        
+        print("convertOriginalToFOVCoordinates - Before combinedMatrix()")
+        let combined = combinedMatrix()
+        print("convertOriginalToFOVCoordinates - After combinedMatrix()")
+        
+        return CGPoint(x: point.x, y: point.y)
+    }
+
 
     
-    func to_nfov(theta: Double, phi: Double) -> UIImage? {
+    func toNFOV(theta: Double, phi: Double) -> UIImage? {
+        print("toNFOV: theta = \(theta), phi = \(phi)")
+        
         guard let originalCGImage = frame.cgImage else {
+            print("toNFOV: Failed to get CGImage from frame")
             return nil
         }
         
@@ -137,32 +141,24 @@ class E2P {
         let equ_w = originalCGImage.width
         let equ_cx = CGFloat((equ_w - 1) / 2)
         let equ_cy = CGFloat((equ_h - 1) / 2)
-        let wFOV: CGFloat = CGFloat(self.focus_of_view)
-        let hFOV = CGFloat(self.height) / CGFloat(self.width) * wFOV
         
         var outImage: UIImage?
         
         if let context = CGContext(
-                data: nil,
-                width: width,
-                height: height,
-                bitsPerComponent: 8,
-                bytesPerRow: 4 * width,
-                space: CGColorSpaceCreateDeviceRGB(),
-                bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
-            ) {
+            data: nil,
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
+            bytesPerRow: 4 * width,
+            space: CGColorSpaceCreateDeviceRGB(),
+            bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+        ) {
             context.interpolationQuality = .high
             
-            for y in 0..<height {
-                for x in 0..<width {
-                    let lon = Double(x) / Double(width) * 360.0 - 180.0
-                    let lat = -Double(y) / Double(height) * 180.0 + 90.0
-                    
-                    // Example usage of equ_cx and equ_cy
-                    let dx = CGFloat(x) - equ_cx
-                    let dy = CGFloat(y) - equ_cy
-                    
-                    let (r, g, b) = interpolateColor(lon: lon, lat: lat)
+            for y in 0..<equ_h {
+                for x in 0..<equ_w {
+                    let originalCoordinates = convertOriginalToFOVCoordinates(x: x, y: y)
+                    let (r, g, b) = interpolateColor(lon: originalCoordinates.x, lat: originalCoordinates.y)
                     
                     context.setFillColor(red: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: 1)
                     context.fill(CGRect(x: x, y: y, width: 1, height: 1))
@@ -178,74 +174,85 @@ class E2P {
 
     
     func to_perspective(theta: Double, phi: Double) -> UIImage? {
-            guard let originalCGImage = frame.cgImage else {
-                return nil
-            }
-
-            let equ_h = originalCGImage.height
-            let equ_w = originalCGImage.width
-
-            // Calculate pFOV and p_hFOV based on width and height
-            let pFOV: CGFloat = CGFloat(self.focus_of_view)
-            let p_hFOV = CGFloat(self.height) / CGFloat(self.width) * pFOV
-
-            var outImage: UIImage?
-
-            if let context = CGContext(
-                data: nil,
-                width: equ_w,
-                height: equ_h,
-                bitsPerComponent: 8,
-                bytesPerRow: 4 * equ_w,
-                space: CGColorSpaceCreateDeviceRGB(),
-                bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
-            ) {
-                context.interpolationQuality = .high
-
-                for y in 0..<equ_h {
-                    for x in 0..<equ_w {
-                        let p_x = CGFloat(x)
-                        let p_y = CGFloat(y)
-
-                        // Calculate lon and lat based on p_x and p_y using inverse transformations
-                        let lon = (p_x / CGFloat(equ_w)) * 360.0 - 180.0
-                        let lat = -(p_y / CGFloat(equ_h)) * 180.0 + 90.0
-
-                        // Apply inverse transformations to get original theta and phi
-                        let lon_normalized = (lon + 180.0) / 360.0
-                        let lat_normalized = (-lat + 90.0) / 180.0
-                        let original_theta = (lon_normalized - 0.5) * CGFloat(wFOV) + 0.5 * CGFloat(wFOV)
-                        let original_phi = -((lat_normalized - 0.5) * CGFloat(hFOV)) + 0.5 * CGFloat(hFOV)
-
-                        let (r, g, b) = interpolateColor(lon: original_theta, lat: original_phi) // Implement this function
-
-                        context.setFillColor(red: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: 1)
-                        context.fill(CGRect(x: x, y: y, width: 1, height: 1))
-                    }
-                }
-
-                if let cgImage = context.makeImage() {
-                    outImage = UIImage(cgImage: cgImage)
-                }
-            }
-
-            return outImage
+        print("to_perspective: theta = \(theta), phi = \(phi)")
+        
+        guard let originalCGImage = frame.cgImage else {
+            print("to_perspective: Failed to get CGImage from frame")
+            return nil
         }
+        
+        let equ_h = originalCGImage.height
+        let equ_w = originalCGImage.width
+        
+        // Calculate pFOV and p_hFOV based on width and height
+        let pFOV: CGFloat = CGFloat(self.focus_of_view)
+        let p_hFOV = CGFloat(self.height) / CGFloat(self.width) * pFOV
+        
+        var outImage: UIImage?
+        
+        if let context = CGContext(
+            data: nil,
+            width: equ_w,
+            height: equ_h,
+            bitsPerComponent: 8,
+            bytesPerRow: 4 * equ_w,
+            space: CGColorSpaceCreateDeviceRGB(),
+            bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+        ) {
+            context.interpolationQuality = .high
+            
+            for y in 0..<equ_h {
+                for x in 0..<equ_w {
+                    let p_x = CGFloat(x)
+                    let p_y = CGFloat(y)
+                    
+                    // Calculate lon and lat based on p_x and p_y using inverse transformations
+                    let lon = (p_x / CGFloat(equ_w)) * 360.0 - 180.0
+                    let lat = -(p_y / CGFloat(equ_h)) * 180.0 + 90.0
+                    
+                    // Apply inverse transformations to get original theta and phi
+                    let lon_normalized = (lon + 180.0) / 360.0
+                    let lat_normalized = (-lat + 90.0) / 180.0
+                    let original_theta = (lon_normalized - 0.5) * CGFloat(wFOV) + 0.5 * CGFloat(wFOV)
+                    let original_phi = -((lat_normalized - 0.5) * CGFloat(hFOV)) + 0.5 * CGFloat(hFOV)
+                    
+                    let (r, g, b) = interpolateColor(lon: original_theta, lat: original_phi) // Implement this function
+                    
+                    context.setFillColor(red: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: 1)
+                    context.fill(CGRect(x: x, y: y, width: 1, height: 1))
+                }
+            }
+            
+            if let cgImage = context.makeImage() {
+                outImage = UIImage(cgImage: cgImage)
+            }
+        }
+        
+        return outImage
+    }
 
 
 
     func interpolateColor(lon: Double, lat: Double) -> (Double, Double, Double) {
-        // Implement your color interpolation logic here based on lon and lat
-          return (lon / 360.0, lat / 180.0, 0.5)
-        //return (0, 0, 0)
+        // Normalize lon and lat to [0, 1] range
+        let lonNormalized = (lon + 180.0) / 360.0
+        let latNormalized = (lat + 90.0) / 180.0
+        
+        // Interpolate color components based on normalized lon and lat
+        let red = lonNormalized
+        let green = latNormalized
+        let blue = 0.5
+        
+        return (red, green, blue)
     }
+
 
 }
 
 struct ContentView: View {
     @State private var originalImage: UIImage?
     @State private var nfovImage: UIImage?
-    let imageName = "ggg" // Replace with your image name
+    let imageName = "panorama1" // Replace with your image name
     
     var body: some View {
         VStack {
@@ -263,9 +270,10 @@ struct ContentView: View {
 
             Button("Convert to NFOV") {
                 if let originalImage = UIImage(named: imageName) {
-                    let e2p = E2P(originalImage: originalImage, fov: 120) // Set the desired FOV
-                    // You need to implement the 'to_nfov' function to return the converted image
-                    nfovImage = e2p.to_nfov(theta: 0, phi: 0) // Set the desired theta and phi angles
+                    let e2p = E2P(originalImage: originalImage, fov: 120)
+                    nfovImage = e2p.toNFOV(theta: 0, phi: 0)
+                } else {
+                    print("somethin wrong")
                 }
             }
             Button("Convert to Perspective") {
